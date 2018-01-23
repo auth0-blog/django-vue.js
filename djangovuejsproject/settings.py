f79bdc6d7f2e2158ccf5c55d95f330f3cfbae000
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-23 21:28:56 -0200

    done!

--- djangovuejsproject/settings.py
+++ djangovuejsproject/settings.py
@@ -13,11 +13,16 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 import json
 from six.moves.urllib import request
+
 from cryptography.x509 import load_pem_x509_certificate
 from cryptography.hazmat.backends import default_backend
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -43,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_jwt',
     'corsheaders',
 ]
@@ -126,21 +132,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'dist'),
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 STATIC_URL = '/static/'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
 }
 
 AUTH0_DOMAIN = 'bk-samples.auth0.com'
-API_IDENTIFIER = 'https://djangovuejs.digituz.com.br'
+API_IDENTIFIER = 'http://djangovuejs.digituz.com.br'
+
+
 PUBLIC_KEY = None
 JWT_ISSUER = None
+
 if AUTH0_DOMAIN:
     jsonurl = request.urlopen('https://' + AUTH0_DOMAIN + '/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read().decode('utf-8'))
@@ -148,8 +165,14 @@ if AUTH0_DOMAIN:
     certificate = load_pem_x509_certificate(cert.encode('utf-8'), default_backend())
     PUBLIC_KEY = certificate.public_key()
     JWT_ISSUER = 'https://' + AUTH0_DOMAIN + '/'
+
+
+
+
 def jwt_get_username_from_payload_handler(payload):
     return 'auth0user'
+
+
 JWT_AUTH = {
     'JWT_PAYLOAD_GET_USERNAME_HANDLER': jwt_get_username_from_payload_handler,
     'JWT_PUBLIC_KEY': PUBLIC_KEY,
@@ -161,4 +184,5 @@ JWT_AUTH = {
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:8080',
+    '127.0.0.1:8080'
 )
