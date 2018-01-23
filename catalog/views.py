3e4228fbc0410d84f4211ec9552ec37e53334e1b
Bruno Krebs <bruno.krebs@auth0.com>
2018-01-23 19:36:21 -0200

    ### Adding Django Views

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.http import HttpResponse
 
-# Create your views here.
+def public(request):
+    return HttpResponse("You don't need to be authenticated to see this")
+
+
+@api_view(['GET'])
+def private(request):
+    return HttpResponse("You should not see this message if not authenticated!");
